83e272f9c831b6dfdaf2633f288fbd95a92b9a40
sohail4612 <sohailshaikh4612@gmail.com>
2025-03-01 16:52:12 +0600

    added current date

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+// toggle page
 document.getElementById("discover").addEventListener("click", function(event){
     event.preventDefault();
     window.location.href="./blog.html"
 })
-
+// theme changer
 let randomNum = function() {
 return Math.floor(Math.random() * 256);
 };
@@ -11,4 +12,12 @@ let changecolor = function(){
     document.body.style.backgroundColor = randomColor;
 };
 document.getElementById("theme").addEventListener("click", changecolor);
+
+// Current Date
+const currentDate = new Date();
+const options = { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' };
+const formattedDate = currentDate.toLocaleDateString('en-US', options);
+const [weekday, month, day, year] = formattedDate.split(' ');
+document.querySelector('.date-day').textContent = weekday;
+document.querySelector('.date-full').textContent = `${month} ${day} ${year}`;
 
