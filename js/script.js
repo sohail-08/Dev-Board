ed7d8f940dee98f2debd09d7a41d88638e92871e
sohail4612 <sohailshaikh4612@gmail.com>
2025-03-01 22:30:54 +0600

    completed button functionality added

--- js/script.js
+++ js/script.js
@@ -18,6 +18,49 @@ const currentDate = new Date();
 const options = { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' };
 const formattedDate = currentDate.toLocaleDateString('en-US', options);
 const [weekday, month, day, year] = formattedDate.split(' ');
-document.querySelector('.date-day').textContent = weekday;
-document.querySelector('.date-full').textContent = `${month} ${day} ${year}`;
+document.querySelector('.date-day').innerText = weekday;
+document.querySelector('.date-full').innerText = `${month} ${day} ${year}`;
+
+// completed button
+const activityLog = document.getElementById("activity-log");
+const button = document.getElementsByClassName("completed-btn");
+const assignedTasks = document.getElementById("assigned-tasks");
+let taskCount = parseInt(assignedTasks.innerText);
+const completedTasks = document.getElementById("completed-tasks");
+let taskNumber = parseInt(completedTasks.innerText);
+
+for(let i=0; i<button.length; i++){
+    button[i].addEventListener("click",function(event){
+        event.preventDefault();
+        alert("Board Updated Successfully");
+
+        taskCount = taskCount - 1;
+        if(taskCount === 0){
+            alert("Congrats!!! You have completed all the current task");
+        }
+        assignedTasks.innerText = taskCount;
+
+        taskNumber =taskNumber + 1;
+        completedTasks.innerText = taskNumber;
+
+        button[i].disabled = true;
+        button[i].style.opacity = 0.2;
+
+        taskTitle = button[i].parentNode.parentNode.parentNode.childNodes[1].childNodes[3].innerText;
+        const time = new Date().toLocaleTimeString();
+        const message = document.createElement('p');
+        message.innerText = `You Have Completed The Task ${taskTitle} at ${time}`;
+        message.style.backgroundColor = "#F4F7FF";
+        message.style.padding = "12px";
+        message.style.borderRadius = "8px";
+        activityLog.appendChild(message);
+     
+
+
+
+    })
+    
+}
+
+
 
